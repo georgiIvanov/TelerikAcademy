ba69054af165713ee0ec9dc7c76381294f99bda2
Georgi <georgi.ivanovtn@gmail.com>
2013-02-24 20:11:15 +0200

    7th and 8th task from Classes part 2

--- OOPHomework/ClassesPart2_Generics/08.GenericMatrix/GenericMatrix.cs
+++ OOPHomework/ClassesPart2_Generics/08.GenericMatrix/GenericMatrix.cs
@@ -10,17 +10,35 @@ namespace _08.GenericMatrix
     {
         static void Main(string[] args)
         {
+            Matrix<double> matrix = new Matrix<double>(3);
+            Random rng = new Random();
 
+            for (int i = 0; i <= matrix.matrix.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= matrix.matrix.GetUpperBound(1); j++)
+                {
+                    matrix.matrix[i, j] = rng.Next(1, 100);
+                }
+            }
+
+            for (int i = 0; i <= matrix.matrix.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= matrix.matrix.GetUpperBound(1); j++)
+                {
+                    Console.Write("{0}\t", matrix.matrix[i,j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 
     class Matrix<T>
     {
-
-    }
-
-    class Lala
-    {
+        public T[,] matrix;
+        public Matrix(int dimensions)
+        {
+            matrix = new T[dimensions, dimensions];
+        }
 
     }
 }
---
+++ OOPHomework/ClassesPart2_Generics/09.AddIndexer/AddIndexer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.GenericMatrix
+{
+    class AddIndexer
+    {
+        static void Main(string[] args)
+        {
+            Matrix<double> matrix = new Matrix<double>(3);
+            Random rng = new Random();
+
+            for (int i = 0; i < matrix.matrixDimension; i++)
+            {
+                for (int j = 0; j < matrix.matrixDimension; j++)
+                {
+                    matrix[i, j] = rng.Next(1, 100);
+                }
+            }
+            Console.WriteLine(matrix[1,1]);
+            
+        }
+    }
+
+    class Matrix<T>
+    {
+        private T[,] matrix;
+        public int matrixDimension
+        {
+            get;
+            set;
+        }
+        public Matrix(int dimensions)
+        {
+            matrix = new T[dimensions, dimensions];
+            matrixDimension = dimensions;
+        }
+
+        public T this[int i, int j]
+        {
+            get { return matrix[i, j]; }
+            set { matrix[i, j] = value; }
+        }
+    }
+}

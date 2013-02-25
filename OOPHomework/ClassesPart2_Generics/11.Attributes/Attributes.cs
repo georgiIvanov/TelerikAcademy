314afee1124900fa08509ebbd37a20340b5725e3
Georgi <georgi.ivanovtn@gmail.com>
2013-02-25 14:00:25 +0200

    Last task from Classes part 2

---
+++ OOPHomework/ClassesPart2_Generics/11.Attributes/Attributes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: AssemblyVersion("2.11")]
+
+namespace _11.Attributes
+{
+    class Attributes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Version of this assembly is: {0}", 
+                Assembly.GetExecutingAssembly().GetName().Version);
+        }
+    }
+}

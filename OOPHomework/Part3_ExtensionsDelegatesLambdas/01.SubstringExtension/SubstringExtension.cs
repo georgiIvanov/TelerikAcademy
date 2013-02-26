c47c0aa7dcc83540907651cd7d837eca4e25edca
Georgi <georgi.ivanovtn@gmail.com>
2013-02-26 19:08:45 +0200

    first 2 tasks from ExtensionsDelegatesLambdaLINQ

---
+++ OOPHomework/Part3_ExtensionsDelegatesLambdas/01.SubstringExtension/SubstringExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class SubstringExtension
+{
+    static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("112233445566");
+
+        Console.WriteLine(sb.Substring(0, 10).ToString());
+    }
+
+    
+}
+
+public static class Extensions
+{
+    public static StringBuilder Substring(this StringBuilder sb, int startIndex, int length)
+    {
+        if (startIndex + length > sb.Length || startIndex < 0 || startIndex > sb.Length)
+        {
+            throw new ArgumentOutOfRangeException("Substring goes otside the bounds of the string");
+        }
+
+        char[] substring = new char[length];
+        for (int i = 0; i < length; i++)
+        {
+            substring[i] = sb[startIndex + i];
+        }
+        StringBuilder newSubstring = new StringBuilder();
+        newSubstring.Append(substring);
+
+        return newSubstring;
+    }
+}
+
---
+++ OOPHomework/Part3_ExtensionsDelegatesLambdas/02.IEnumerableExtensions/IEnumerableExtensions.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.IEnumerableExtensions
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<double> numbers = new List<double>();
+            numbers.Add(2.3);
+            numbers.Add(1);
+            numbers.Add(3);
+
+            Console.WriteLine(numbers.GetSum());
+            Console.WriteLine(numbers.GetProduct());
+            Console.WriteLine(numbers.GetMin());
+            Console.WriteLine(numbers.GetMax());
+            Console.WriteLine(numbers.GetAverage());
+        }
+    }
+
+    public static class IEnumerableExtensions
+    {
+        public static T GetSum<T> (this IEnumerable<T> collection) 
+        {
+            dynamic sum = 0;
+            foreach (var item in collection)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+
+        public static T GetProduct<T>(this IEnumerable<T> collection)
+        {
+            dynamic product = 1;
+            foreach (var item in collection)
+            {
+                product *= item;
+            }
+            return product;
+        }
+
+        public static T GetMin<T>(this IEnumerable<T> collection)
+        {
+            dynamic min = collection.First();
+            foreach (var item in collection)
+            {
+                if (item < min)
+                {
+                    min = item;
+                }
+            }
+            return min;
+        }
+
+        public static T GetMax<T>(this IEnumerable<T> collection)
+        {
+            dynamic max = collection.First();
+            foreach (var item in collection)
+            {
+                if (item > max)
+                {
+                    max = item;
+                }
+            }
+            return max;
+        }
+
+        public static T GetAverage<T>(this IEnumerable<T> collection)
+        {
+            int count = 0;
+            dynamic sum = 0;
+
+            foreach (var item in collection)
+            {
+                sum += item;
+                count++;
+            }
+            sum = sum / count;
+            return sum;
+        }
+    }
+}

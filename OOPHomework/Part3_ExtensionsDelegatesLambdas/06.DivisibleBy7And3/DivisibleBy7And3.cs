065db55c24c87685361aa7831ddfa0f90d40cf76
Georgi <georgi.ivanovtn@gmail.com>
2013-02-27 04:31:41 +0200

    2 tasks

---
+++ OOPHomework/Part3_ExtensionsDelegatesLambdas/06.DivisibleBy7And3/DivisibleBy7And3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.DivisibleBy7And3
+{
+    class DivisibleBy7And3
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 42, 49, 21, 63, 189 };
+
+            var divisible = numbers.Where(x => x % 3 == 0 && x % 7 == 0);
+
+            foreach (var item in divisible)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            var divisible2 = from num in numbers
+                             where num % 3 == 0 && num % 7 == 0
+                             select num;
+
+            foreach (var item in divisible2)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ OOPHomework/Part3_ExtensionsDelegatesLambdas/07.Timer/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace _07.Timer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer.TimerDel timerHandler = DoImportantThings;
+            Timer.sw.Start();
+
+            while (true)
+            {
+                Timer.DoStuffEveryTTicks(1200000, timerHandler);
+            }
+
+        }
+
+        public static void DoImportantThings()
+        {
+            Console.WriteLine("Lalala");
+        }
+        
+    }
+
+    static class Timer
+    {
+        public static Stopwatch sw = new Stopwatch();
+        
+        public static void DoStuffEveryTTicks(int t, TimerDel importantMethod)
+        {
+            if (sw.ElapsedTicks > t)
+            {
+                importantMethod();
+                sw.Restart();
+            }
+        }
+        public delegate void TimerDel();
+    }
+}

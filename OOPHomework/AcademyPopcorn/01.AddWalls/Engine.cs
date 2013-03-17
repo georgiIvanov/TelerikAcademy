56f19927f32f8b26fe401b76b9ed48b5c1d9fcfb
Georgi <georgi.ivanovtn@gmail.com>
2013-03-17 15:35:21 +0200

    first three tasks from AcademyPopcorn Homework

---
+++ OOPHomework/AcademyPopcorn/01.AddWalls/Engine.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AcademyPopcorn
+{
+    public class Engine
+    {
+        IRenderer renderer;
+        IUserInterface userInterface;
+        List<GameObject> allObjects;
+        List<MovingObject> movingObjects;
+        List<GameObject> staticObjects;
+        Racket playerRacket;
+
+        public Engine(IRenderer renderer, IUserInterface userInterface)
+        {
+            this.renderer = renderer;
+            this.userInterface = userInterface;
+            this.allObjects = new List<GameObject>();
+            this.movingObjects = new List<MovingObject>();
+            this.staticObjects = new List<GameObject>();
+        }
+
+        private void AddStaticObject(GameObject obj)
+        {
+            this.staticObjects.Add(obj);
+            this.allObjects.Add(obj);
+        }
+
+        private void AddMovingObject(MovingObject obj)
+        {
+            this.movingObjects.Add(obj);
+            this.allObjects.Add(obj);
+        }
+
+        public virtual void AddObject(GameObject obj)
+        {
+            if (obj is MovingObject)
+            {
+                this.AddMovingObject(obj as MovingObject);
+            }
+            else
+            {
+                if (obj is Racket)
+                {
+                    AddRacket(obj);
+
+                }
+                else
+                {
+                    this.AddStaticObject(obj);
+                }
+            }
+        }
+
+        private void AddRacket(GameObject obj)
+        {
+            //TODO: we should remove the previous racket from this.allObjects
+            this.playerRacket = obj as Racket;
+            this.AddStaticObject(obj);
+        }
+
+        public virtual void MovePlayerRacketLeft()
+        {
+            this.playerRacket.MoveLeft();
+        }
+
+        public virtual void MovePlayerRacketRight()
+        {
+            this.playerRacket.MoveRight();
+        }
+
+        public virtual void Run()
+        {
+            while (true)
+            {
+                this.renderer.RenderAll();
+
+                System.Threading.Thread.Sleep(50);
+
+                this.userInterface.ProcessInput();
+
+                this.renderer.ClearQueue();
+
+                foreach (var obj in this.allObjects)
+                {
+                    obj.Update();
+                    this.renderer.EnqueueForRendering(obj);
+                }
+
+                CollisionDispatcher.HandleCollisions(this.movingObjects, this.staticObjects);
+
+                List<GameObject> producedObjects = new List<GameObject>();
+
+                foreach (var obj in this.allObjects)
+                {
+                    producedObjects.AddRange(obj.ProduceObjects());
+                }
+
+                this.allObjects.RemoveAll(obj => obj.IsDestroyed);
+                this.movingObjects.RemoveAll(obj => obj.IsDestroyed);
+                this.staticObjects.RemoveAll(obj => obj.IsDestroyed);
+
+                foreach (var obj in producedObjects)
+                {
+                    this.AddObject(obj);
+                }
+            }
+        }
+    }
+}
---
+++ OOPHomework/AcademyPopcorn/AcademyPopcorn/AcademyPopcornMain.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademyPopcorn
+{
+    class AcademyPopcornMain
+    {
+        const int WorldRows = 23;
+        const int WorldCols = 40;
+        const int RacketLength = 6;
+
+        static void Initialize(Engine engine)
+        {
+            int startRow = 3;
+            int startCol = 2;
+            int endCol = WorldCols - 2;
+
+            for (int i = startCol; i < endCol; i++)
+            {
+                Block currBlock = new Block(new MatrixCoords(startRow, i));
+
+                engine.AddObject(currBlock);
+            }
+
+            Ball theBall = new Ball(new MatrixCoords(WorldRows / 2, 0),
+                new MatrixCoords(-1, 1));
+
+            engine.AddObject(theBall);
+
+            Racket theRacket = new Racket(new MatrixCoords(WorldRows - 1, WorldCols / 2), RacketLength);
+
+            engine.AddObject(theRacket);
+        }
+
+        static void Main(string[] args)
+        {
+            IRenderer renderer = new ConsoleRenderer(WorldRows, WorldCols);
+            IUserInterface keyboard = new KeyboardInterface();
+
+            Engine gameEngine = new Engine(renderer, keyboard);
+
+            keyboard.OnLeftPressed += (sender, eventInfo) =>
+            {
+                gameEngine.MovePlayerRacketLeft();
+            };
+
+            keyboard.OnRightPressed += (sender, eventInfo) =>
+            {
+                gameEngine.MovePlayerRacketRight();
+            };
+
+            Initialize(gameEngine);
+
+            //
+
+            gameEngine.Run();
+        }
+    }
+}

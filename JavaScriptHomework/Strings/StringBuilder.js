a7d7f419b763e497f8db0ba721001aa954a347f6
Georgi <georgi.ivanovtn@gmail.com>
2013-03-30 23:04:06 +0200

    JS strings homework 1 to 9

---
+++ JavaScriptHomework/Strings/StringBuilder.js
@@ -0,0 +1,26 @@
+﻿function buildStringBuilder() {
+    return {
+        string: [],
+        len: 0,
+        append: function (str) {
+            this.string[this.len++] = str;
+            return this;
+        },
+        toString: function () {
+            return this.string.join("");
+        },
+        Reverse: function () {
+            var reversed = "";
+            for (var i = this.string.length - 1; i >= 0; i--) {
+                for (var j = this.string[i].length - 1; j >= 0; j--) {
+                    reversed += this.string[i].charAt(j);
+                }
+            }
+            return reversed;
+        },
+        Clear: function () {
+            this.string = [];
+            this.len = 0;
+        }
+    };
+}

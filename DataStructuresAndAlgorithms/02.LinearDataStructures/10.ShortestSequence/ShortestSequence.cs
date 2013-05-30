bff2f07ebe1f639def892cd58fd20a9a233e071f
Georgi <georgi.ivanovtn@gmail.com>
2013-05-30 14:36:05 +0300

    task 10, linear data structures homework

---
+++ DataStructuresAndAlgorithms/02.LinearDataStructures/10.ShortestSequence/ShortestSequence.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace _10.ShortestSequence
+{
+    class ShortestSequence
+    {
+        static List<int> DetermineShortestSequence(Queue<List<int>> queueList, int target)
+        {
+            List<int> result;
+
+            while (true)
+            {
+                List<int> currentList = queueList.Dequeue();
+                int currentLastElement = currentList[currentList.Count - 1];
+
+                int plusOneValue = currentLastElement + 1;
+                List<int> plusOneList = new List<int>(currentList);
+                plusOneList.Add(plusOneValue);
+                if (plusOneValue < target)
+                {
+                    queueList.Enqueue(plusOneList);
+                }
+                else
+                {
+                    result = plusOneList;
+                    break;
+                }
+
+                int plusTwoValue = currentLastElement + 2;
+                List<int> plusTwoList = new List<int>(currentList);
+                plusTwoList.Add(plusTwoValue);
+                if (plusTwoValue < target)
+                {
+                    queueList.Enqueue(plusTwoList);
+                }
+                else
+                {
+                    result = plusTwoList;
+                    break;
+                }
+
+                int multiplyByTwoValue = 2 * currentLastElement;
+                List<int> multiplyByTwoList = new List<int>(currentList);
+                multiplyByTwoList.Add(multiplyByTwoValue);
+                if (multiplyByTwoValue < target)
+                {
+                    queueList.Enqueue(multiplyByTwoList);
+                }
+                else if (multiplyByTwoValue == target)
+                {
+                    result = multiplyByTwoList;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        static void Main()
+        {
+            int n = 5;
+            int m = 16;
+
+            Queue<List<int>> queueList = new Queue<List<int>>();
+            queueList.Enqueue(new List<int>(new int[] {n}));
+
+            List<int> result = DetermineShortestSequence(queueList, m);
+
+            Console.Write("Result: ");
+            for (int i = 0; i < result.Count; i++)
+            {
+                if (i < result.Count - 1)
+                {
+                    Console.Write("{0}->", result[i]);
+                }
+                else
+                {
+                    Console.WriteLine("{0}", result[i]);
+                }
+            }
+        }
+    }
+
+}
---
+++ DataStructuresAndAlgorithms/02.LinearDataStructures/10.ShortestSequence/delete.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _10_ShortestSequOfOperators
+{
+    class MinOperators
+    {
+        static List<int> FindMinOperations(Queue<List<int>> queueList, int M)
+        {
+            while (true)
+            {
+                List<int> currentList = queueList.Dequeue();
+                int currentLastElement = currentList[currentList.Count - 1];
+
+                int firstNextValue = currentLastElement + 1;
+                List<int> firstNextList = new List<int>(currentList);
+                firstNextList.Add(firstNextValue);
+                if (firstNextValue < M)
+                {
+                    queueList.Enqueue(firstNextList);
+                }
+                else if (firstNextValue == M)
+                {
+                    queueList.Enqueue(firstNextList);
+                    return firstNextList;
+                }
+
+                int secondNextValue = currentLastElement + 2;
+                List<int> secondNextList = new List<int>(currentList);
+                secondNextList.Add(secondNextValue);
+                if (secondNextValue < M)
+                {
+                    queueList.Enqueue(secondNextList);
+                }
+                else if (secondNextValue == M)
+                {
+                    queueList.Enqueue(secondNextList);
+                    return secondNextList;
+                }
+
+                int thirdNextValue = currentLastElement * 2;
+                List<int> thirdNextList = new List<int>(currentList);
+                thirdNextList.Add(thirdNextValue);
+                if (thirdNextValue < M)
+                {
+                    queueList.Enqueue(thirdNextList);
+                }
+                else if (thirdNextValue == M)
+                {
+                    queueList.Enqueue(thirdNextList);
+                    return thirdNextList;
+                }
+            }
+
+        }
+        static void aa(string[] args)
+        {
+            int n = 3;
+            int m = 23;
+
+            List<int> firstList = new List<int>();
+            firstList.Add(n);
+
+            Queue<List<int>> solutions = new Queue<List<int>>();
+            solutions.Enqueue(firstList);
+
+
+            List<int> result = FindMinOperations(solutions, m);
+
+            Console.Write("Result: ");
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                if (i < result.Count - 1)
+                {
+                    Console.Write("{0}->", result[i]);
+                }
+                else
+                {
+                    Console.WriteLine("{0}", result[i]);
+                }
+            }
+        }
+    }
+}

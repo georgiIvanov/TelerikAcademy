c7af0d37b575efa31a048547414199442d87d248
Georgi <georgi.ivanovtn@gmail.com>
2013-06-07 20:36:52 +0300

    03.WordOccurance, Trie, compared with hashtable

---
+++ DataStructuresAndAlgorithms/05.AdvancedDataStructures/03.WordOccurance/Trie.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.WordOccurance
+{
+    class TrieNode
+    {
+        int words;
+        int prefixes;
+        TrieNode[] edges;
+
+        public TrieNode()
+        {
+            this.edges = new TrieNode[26];
+            this.Words = 0;
+            this.Prefixes = 0;
+        }
+
+        public TrieNode AddWord(TrieNode node, string word)
+        {
+            return this.AddWord(node, word, 0);
+        }
+
+        public int CountWords(TrieNode node, string word)
+        {
+            return this.CountWords(node, word, 0);
+        }
+
+        public int CountPrefix(TrieNode node, string word)
+        {
+            return this.CountPrefix(node, word, 0);
+        }
+
+        public int Words
+        {
+            get
+            {
+                return this.words;
+            }
+            set
+            {
+                this.words = value;
+            }
+        }
+
+        public int Prefixes
+        {
+            get
+            {
+                return this.prefixes;
+            }
+            set
+            {
+                this.prefixes = value;
+            }
+        }
+
+        private TrieNode AddWord(TrieNode node, string word, int indexInWord)
+        {
+            if (word.Length == indexInWord)
+            {
+                node.Words += 1;
+            }
+            else
+            {
+                node.Prefixes += 1;
+                
+                int index = word[indexInWord] - 'a';
+                indexInWord++;
+
+                if (node.edges[index] == null)
+                {
+                    node.edges[index] = new TrieNode();
+                }
+
+                node.edges[index] = AddWord(node.edges[index], word, indexInWord);
+            }
+
+            return node;
+        }
+
+        private int CountWords(TrieNode node, string word, int indexInWord)
+        {
+            if (word.Length == indexInWord)
+            {
+                return node.words;
+            }
+            else
+            {
+                int k = word[indexInWord] - 'a';
+                indexInWord++;
+                if (node.edges[k] == null)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return CountWords(node.edges[k], word, indexInWord);
+                }
+            }
+        }
+
+        private int CountPrefix(TrieNode node, string word, int indexInWord)
+        {
+            if (word.Length == indexInWord)
+            {
+                return node.prefixes;
+            }
+            else
+            {
+                int k = word[indexInWord] - 'a';
+                indexInWord++;
+                if (node.edges[k] == null)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return CountPrefix(node.edges[k], word, indexInWord);
+                }
+            }
+        }
+
+    }
+}
---
+++ DataStructuresAndAlgorithms/05.AdvancedDataStructures/03.WordOccurance/WordOccurance.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace _03.WordOccurance
+{
+    class WordOccurance
+    {
+        static void Main(string[] args)
+        {
+            ///===========================================
+            ///Unzip the archive in the project directory
+            ///===========================================
+            
+            Stopwatch sw = new Stopwatch();
+            TrieNode start = new TrieNode();
+            var words = SetInputText();
+            
+            //takes about 9 secs
+            SearchInTrie(sw, start, words, "again");
+
+            SearchInDictionary(sw, words, "again");
+        }
+
+        private static void SearchInTrie(Stopwatch sw, TrieNode start, MatchCollection words, string searchedWord)
+        {
+            int wordCount = 0;
+            sw.Start();
+            foreach (var item in words)
+            {
+                start.AddWord(start, item.ToString());
+            }
+            sw.Stop();
+            Console.WriteLine("Time to populate the trie: {0}", sw.Elapsed);
+
+
+            sw.Restart();
+            wordCount = start.CountWords(start, searchedWord);
+            sw.Stop();
+
+            Console.WriteLine("Times word found: {0}", wordCount);
+            Console.WriteLine("Elapsed time in searching: {0}", sw.Elapsed);
+        }
+
+        private static void SearchInDictionary(Stopwatch sw, MatchCollection words, string searchedWord)
+        {
+            int wordCount = 0;
+            Dictionary<string, int> wordsInDictionary = new Dictionary<string, int>();
+
+            sw.Restart();
+            foreach (var item in words)
+            {
+                string word = item.ToString();
+                if (wordsInDictionary.ContainsKey(word))
+                {
+                    wordsInDictionary[word] += 1;
+                }
+                else
+                {
+                    wordsInDictionary[word] = 1;
+                }
+            }
+            sw.Stop();
+            Console.WriteLine("\n\nTime to populate dictionary: {0}", sw.Elapsed);
+
+            sw.Restart();
+            wordCount = wordsInDictionary[searchedWord];
+            sw.Stop();
+
+            Console.WriteLine("Times word found: {0}", wordCount);
+            Console.WriteLine("Elapsed time in searching: {0}", sw.Elapsed);
+        }
+
+        static MatchCollection SetInputText()
+        {
+            ///===========================================
+            ///Unzip the archive in the project directory
+            ///===========================================
+            string inputText;
+            StreamReader sr = new StreamReader("..\\..\\text.txt");
+            using (sr)
+            {
+                inputText = sr.ReadToEnd().ToLower();
+            }
+
+            var matches = Regex.Matches(inputText, @"[a-zA-Z]+");
+            return matches;
+        }
+    }
+}

6daa49c6018c8d6f9f3483942c47cfa5d3f98286
Georgi <georgi.ivanovtn@gmail.com>
2013-06-08 12:14:12 +0300

    02.PriceRange from 06.DSEfficiency

---
+++ DataStructuresAndAlgorithms/06.DataStructuresEfficiency/02.PriceRange/Article.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.PriceRange
+{
+    struct Article : IComparable<Article>
+    {
+        int barcode;
+        double price;
+        string vendor;
+        string title;
+
+        public Article(int barcode, double price, string vendor, string title)
+        {
+            this.barcode = barcode;
+            this.price = price;
+            this.vendor = vendor;
+            this.title = title;
+        }
+
+        public int CompareTo(Article other)
+        {
+            return this.price.CompareTo(other.price);
+        }
+
+        public int Barcode
+        {
+            get
+            {
+                return this.barcode;
+            }
+            set
+            {
+                this.barcode = value;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                this.price = value;
+            }
+        }
+
+        public string Vendor
+        {
+            get
+            {
+                return this.vendor;
+            }
+            set
+            {
+                this.vendor = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return this.title;
+            }
+            set
+            {
+                this.title = value;
+            }
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/06.DataStructuresEfficiency/02.PriceRange/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wintellect.PowerCollections;
+
+namespace _02.PriceRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderedMultiDictionary<double, Article> articles = new OrderedMultiDictionary<double, Article>(true);
+
+            //this takes a few seconds
+            for (int i = 0; i < 2000000; i++)
+            {
+                double price = i / 100.3;
+                Article article = new Article(i * 971, Math.Round(price, 2), i.ToString(), i.ToString());
+                articles.Add(new KeyValuePair<double,
+                ICollection<Article>>(price, new Article[] { article }));
+            }
+
+            foreach (var node in GetArticlesInPriceRange(articles, 1d, 3d))
+            {
+                foreach (var item in node.Value)
+                {
+                    Console.WriteLine("{0}: {1}",item.Title, item.Price );
+                }
+            }
+        }
+
+        static OrderedMultiDictionary<double, Article>.View GetArticlesInPriceRange(OrderedMultiDictionary<double, Article> articles, double low, double high)
+        {
+            var aa = articles.Range(low, true, high, false);
+            return aa;
+        }
+    }
+}

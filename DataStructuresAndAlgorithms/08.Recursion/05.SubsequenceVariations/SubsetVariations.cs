2e44a4f69decfdffd26f6d625fd45a273f8ed2c8
Georgi <georgi.ivanovtn@gmail.com>
2013-06-19 12:50:34 +0300

    subsetVariations with recursion

--- DataStructuresAndAlgorithms/08.Recursion/05.SubsequenceVariations/SubsetVariations.cs
+++ DataStructuresAndAlgorithms/08.Recursion/05.SubsequenceVariations/SubsetVariations.cs
@@ -10,7 +10,36 @@ namespace _05.SubsequenceVariations
     {
         static void Main(string[] args)
         {
+            int k = 2;
+            string[] set = new string[] { "hi", "a", "b" };
+            int[] variationIndexes = new int[set.Length];
 
+            Variate(0, k, set.Length, set, variationIndexes);
+        }
+
+        static void Variate(int i, int k, int n, string[] set, int[] varIndexes)
+        {
+            if (i >= k)
+            {
+                Print(k, set, varIndexes);
+                return;
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                varIndexes[i] = j;
+                Variate(i + 1, k, n, set, varIndexes);
+            }
+
+        }
+
+        static void Print(int n, string[] set, int[] varIndexes)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", set[varIndexes[i]]);
+            }
+            Console.WriteLine();
         }
     }
 }

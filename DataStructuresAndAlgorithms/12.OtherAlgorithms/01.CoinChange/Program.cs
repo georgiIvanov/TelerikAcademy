def4c2aba49fcdf347bd67ffd500b2da2e935161
Georgi <georgi.ivanovtn@gmail.com>
2013-06-22 18:28:30 +0300

    12.OtherAlgorithms homework
    task 1 - CoinChange
    task 2 - PointInTriangle

---
+++ DataStructuresAndAlgorithms/12.OtherAlgorithms/01.CoinChange/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.CoinChange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startCoins = 33;
+            int[] change = new int[3];
+            //indices 0, 1, 2 -> 5st, 2st, 1st
+
+            GetChange(ref startCoins, change);
+
+
+            Console.WriteLine("{0} coins x5", change[0]);
+            Console.WriteLine("{0} coins x2", change[1]);
+            Console.WriteLine("{0} coins x1", change[2]);
+        }
+
+        static void GetChange(ref int coins, int[] change)
+        {
+            if (coins >= 5)
+            {
+                change[0] += 1;
+                coins -= 5;
+            }
+            else if (coins >= 2)
+            {
+                change[1] += 1;
+                coins -= 2;
+            }
+            else if (coins >= 1)
+            {
+                change[2] += 1;
+                coins -= 1;
+            }
+
+            if (coins > 0)
+            {
+                GetChange(ref coins, change);
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/12.OtherAlgorithms/02.PointInTriangle/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.DirectX;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace _02.PointInTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3d vectors are necessary, because crossproduct creates a 
+            //perpendicular vector, which doesnt exist in 2d space
+            Vector3 a = new Vector3(1f, 1f, 0f);
+            Vector3 b = new Vector3(3f, 1f, 0f);
+            Vector3 c = new Vector3(2f, 3f, 0f);
+
+            Vector3 x = new Vector3(2f, 2f, 0f);
+
+            bool result = PointInTriangle(x, a, b, c);
+
+            if (result)
+            {
+                Console.WriteLine("{0}is in triangle", x.ToString());
+            }
+            else
+            {
+                Console.WriteLine("{0}is NOT in triangle", x.ToString());
+            }
+        }
+
+        static bool PointInTriangle(Vector3 p, Vector3 a, Vector3 b, Vector3 c)
+        {
+            if (SameSide(p, a, b, c) && SameSide(p, b, a, c) && SameSide(p, c, a, b))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static bool SameSide(Vector3 p1, Vector3 p2, Vector3 a, Vector3 b)
+        {
+            Vector3 cp1 = Vector3.Cross(Vector3.Subtract(b, a), Vector3.Subtract(p1, a));
+            Vector3 cp2 = Vector3.Cross(Vector3.Subtract(b, a), Vector3.Subtract(p2, a));
+
+            if (Vector3.Dot(cp1, cp2) >= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}

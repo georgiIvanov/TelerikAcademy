a05c23ac04b79155525735de24476772999652bd
Georgi <georgi.ivanovtn@gmail.com>
2013-06-30 22:34:57 +0300

    facebook api task

---
+++ JavaScriptApps/05.ExternalAPIs/03-6.FacebookUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("_03_6.FacebookUI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("_03_6.FacebookUI")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5073706d-2f2c-4a0e-8ac4-c8982d8e1acc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ JavaScriptApps/05.ExternalAPIs/03-6.FacebookUI/facebook.js
@@ -0,0 +1,75 @@
+﻿var token;
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '166748376839883',
+        channelUrl: '//http://localhost:21851/index.html',
+        status: true, // check login status
+        cookie: true, // enable cookies to allow the server to access the session
+        xfbml: true // parse XFBML
+    });
+
+    FB.login(function (response) {
+        if (response.authResponse) {
+            token = response.authResponse.accessToken;
+            getProfileInfo();
+            getFriends();
+            $('#fbLogout').css("display", "inline-block");
+        } else {
+            console.log('User cancelled login or did not fully authorize.');
+        }
+    }, { scope: 'read_friendlists,user_photos, user_birthday, user_location' });
+};
+
+(function (d) {
+    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement('script'); js.id = id; js.async = true;
+    js.src = "//connect.facebook.net/en_US/all.js";
+    ref.parentNode.insertBefore(js, ref);
+}(document));
+
+
+function getProfileInfo() {
+    FB.api('/me', function (response) {
+        var holder = $("#profile");
+        var name = response.name;
+        var birthday = response.birthday;
+        var location = response.location.name;
+        var url = "https://graph.facebook.com/" + response.id + "/picture";
+        holder.append("<img src =" + url + "/>");
+        holder.append("<p>" + name + "</p>");
+        holder.append("<p>" + birthday + "</p>");
+        holder.append("<p>" + location + "</p>");
+    });
+    $("#log").css("display", "none");
+}
+
+function getFriends() {
+    FB.api('/me/friends?access_token=' + token, function (response) {
+        var friendsHolder = $('#friends');
+        for (i = 0; i < response.data.length; i++) {
+            var friendPictureUrl = 'https://graph.facebook.com/' + response.data[i].id + '/picture?width=200&height=200';
+            var friendName = response.data[i].name;
+            friendsHolder.append("<img src =" + friendPictureUrl + " title=" + friendName + "/>");
+        }
+    });
+}
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=395337783908870";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function fbLogout() {
+    FB.logout(function (response) {
+        $('#disconnect').hide('slow');
+        $('#profile').hide('slow');
+        $('#friends').hide('slow');
+        $('#log').show();
+        $('#fbLogout').css("display", "none");
+        window.location.reload();
+    });
+}

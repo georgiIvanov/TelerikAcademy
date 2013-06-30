5fde08b311af6c324b4d1447bb6ed6c8728cde59
Georgi <georgi.ivanovtn@gmail.com>
2013-06-30 13:16:05 +0300

    Online map tour task from jsApps course

---
+++ JavaScriptApps/05.ExternalAPIs/01.OnlineMapTourism/capitalsData.js
@@ -0,0 +1,42 @@
+﻿[{
+    "info": "<p>The city has a population of 1.4 million inhabitants. Spoken language is Bulgarian which is closely related to Russian.Sofia is the administrative, cultural, economic, and educational center of the country.</p>",
+    "lat": 42.709664695829426,
+    "long": 23.32630843017570,
+    "title": "Sofia, Bulgaria",
+    "color": "green"
+},
+  {
+    "info": "<p>The city is the country's industrial and financial center and has a population of about 2 million inhabitants.Bucharest is seat of the Romanian Government. Just zoom in (+) to see the Palace of the Parliament, the world's largest civilian administrative building.</p>",
+      "lat": 44.45208501135689,
+      "long": 26.122718505859375,
+      "title": "Bucharest, Romania",
+      "color": "yellow"
+  },
+  {
+      "info": "<p>Satellite view is showing Kiev, largest city and the national capital of  Ukraine, located on both sides of the Dnieper River in the north central part of the country. Kiev has a population of about 2.66 million inhabitants. Official language is Ukrainian. The map shows a city map of Kiev with expressways, main roads and streets, as well as the location of Zhulhany Airport (IATA code: IEV). Zoom out to find the location of Boryspil International Airport (IATA code: KBP) about 29 km (18 mi) southeast of the city center. </p>",
+      "lat": 50.42713841743867,
+      "long": 30.541555786132812,
+      "title": "Kiev, Ukraine",
+      "color": "red"
+  },
+  {
+      "info": "<p>Satellite view is showing Minsk, largest city and the national capital of  Belarus, situated on the Svislach and Niamiha (not visible, because Niamiha is in a pipe) rivers. Minsk has a population of 1.8 million inhabitants. Spoken languages are Russian (predominantly) and Belarusian.</p>",
+      "lat": 53.928000167735165,
+      "long": 27.6003515625,
+      "title": "Minsk, Belarus",
+      "color": "blue"
+  },
+  {
+      "info": "<p>The city has a population of 1.7 million inhabitants, 2.6 million people live in its metropolitan area. Spoken language is Polish. Warsaw is regarded as an Alpha City and it is the political and cultural center of the country. The Historic Centre of Warsaw is a UNESCO World Heritage site.</p>",
+      "lat": 52.22168505384983,
+      "long": 21.01796875,
+      "title": "Warsaw, Poland",
+      "color": "purple"
+  },
+  {
+      "info": "<p>Moscow became the capital when Ivan the Terrible proclaimed himself the first tsar in the 16th century. Peter the Great moved his capital to St. Petersburg in 1712, but, after the Bolshevik Revolution of 1917, Moscow was made the capital of the Soviet Union and seat of the new Soviet government, with its center in the Kremlin. Today the city is the country’s political, economical, and financial center. In its metropolitan area there live more than 10 million people.</p>",
+      "lat": 55.72014932919687,
+      "long": 37.75726318359375,
+      "title": "Moscow, Russia",
+      "color": "orange"
+  }]
---
+++ JavaScriptApps/05.ExternalAPIs/01.OnlineMapTourism/maps.js
@@ -0,0 +1,106 @@
+﻿(function ($) {
+    var map;
+    var capitals;
+    var counter;
+
+    function initialize() {
+        var mapOptions = {
+            zoom: 6,
+            center: new google.maps.LatLng(42.69959515809203, 23.337364196777344),
+            mapTypeId: google.maps.MapTypeId.TERRAIN
+        };
+
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+
+        capitals = [];
+        counter = 0;
+        $.ajaxSetup({ "async": false });
+        $.getJSON("capitalsData.js", function (data) {
+            for (var i = 0; i < data.length; i++) {
+                var newMarker = new MapMarker(data[i].lat, data[i].long, data[i].info, data[i].title, data[i].color);
+                capitals.push(newMarker);
+            }
+        });
+        var navContainer = $('<div id="nav"></div>');
+        var nav = $("<ul/>");
+        for (var i = 0; i < capitals.length; i++) {
+            var item = $('<li></li>');
+            item.append(capitals[i].title);
+            item.bind('click', { marker: capitals[i], current: i }, function (event) {
+                var data = event.data;
+                counter = data.current;
+                map.panTo((data.marker).getPosition());
+            });
+            nav.append(item);
+        };
+
+        $('body').append(navContainer.append(nav));
+
+        var prevbtn = $('<a class="NavButton">Prev</a>');
+        var nextbtn = $('<a class="NavButton">Next</a>');
+        prevbtn.attr('id', 'btn-prev');
+        nextbtn.attr('id', 'btn-next');
+        prevbtn.on('click', previous);
+        nextbtn.on('click', next);
+
+        $('#nav').append(prevbtn);
+        $('#nav').append(nextbtn);
+        
+    }
+
+    function next() {
+        if (counter < capitals.length - 1) {
+            counter += 1;
+        } else {
+            counter = 0;
+        }
+        map.panTo(capitals[counter].getPosition());
+    }
+
+    function previous() {
+        if (counter > 0) {
+            counter -= 1;
+        } else {
+            counter = capitals.length - 1;
+        }
+        map.panTo(capitals[counter].getPosition());
+    }
+
+    var MapMarker = Class.create({
+        init: function (latitude, longitude, info, title, color) {
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.info = info;
+            this.title = title;
+            this.color = color;
+            this.marker = this.createMarker();
+        },
+        createMarker: function () {
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(this.latitude, this.longitude),
+                map: map,
+                title: this.title
+            });
+            iconFile = 'http://maps.google.com/mapfiles/ms/icons/' + this.color + '-dot.png';
+            marker.setIcon(iconFile);
+
+            var infowindow = new google.maps.InfoWindow({
+                content: this.info
+            });
+
+            google.maps.event.addListener(marker, 'click', function () {
+                infowindow.open(map, marker);
+                map.panTo(marker.getPosition());
+            });
+
+            return marker;
+        },
+        getPosition: function () {
+            return this.marker.getPosition();
+        }
+
+    });
+
+    google.maps.event.addDomListener(window, 'load', initialize());
+})(jQuery);

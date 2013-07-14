46909cc48261f7cb021dba5a843dc84f98427902
Georgi <georgi.ivanovtn@gmail.com>
2013-07-14 22:45:09 +0300

    ADO.NET task 10 - create and use SQLite table

---
+++ DatabaseSystemsCourse/07.ADO.NET/10.SQLLite/SQLLite.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Globalization;
+using System.Threading;
+using System.Data;
+
+namespace _10.SQLLite
+{
+    class SQLLite
+    {
+        static SQLiteDatabase db;
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            db = new SQLiteDatabase("Books");
+
+            // Method used only once to create books table
+            // CreateBooksTable();
+
+
+            // Uncomment methods to test
+
+            //AddBook("SQLite For Professionals", "Homer Simpson", DateTime.Parse("7/14/2013"), 666);
+
+            //ListBooks();
+
+            SearchBook("A Game Of Thrones");
+        }
+
+        
+
+        public static void AddBook(string title, string author, DateTime pubDate, int ISBN)
+        {
+            Dictionary<string, string> data = new Dictionary<string, string>();
+            data.Add("TITLE", title);
+            data.Add("AUTHOR", author);
+            data.Add("PUBDATE", pubDate.ToShortDateString());
+            data.Add("ISBN", Convert.ToString(ISBN));
+
+            db.Insert("Books", data);
+        }
+
+        public static void ListBooks()
+        {
+            string query = "select TITLE \"Title\", AUTHOR \"Author\", " +
+                "PUBDATE \"Publish Date\", ISBN \"ISBN\" from Books";
+
+            DataTable books = db.GetDataTable(query);
+
+            foreach (DataRow r in books.Rows)
+            {
+
+                Console.WriteLine("{0}, {1}, {2}, {3}",r["Title"].ToString(), 
+                    r["Author"].ToString(),
+                    r["Publish Date"].ToString(),
+                    r["ISBN"].ToString());
+
+            }
+        }
+
+        public static void SearchBook(string title)
+        {
+            string query = "select TITLE \"Title\", AUTHOR \"Author\", " +
+                "PUBDATE \"Publish Date\", ISBN \"ISBN\" from Books ";
+
+            DataTable books = db.GetDataTable(query);
+
+            foreach (DataRow r in books.Rows)
+            {
+                if ((string)r["Title"] == title)
+                {
+                    Console.WriteLine("{0}, {1}, {2}, {3}", r["Title"].ToString(),
+                        r["Author"].ToString(),
+                        r["Publish Date"].ToString(),
+                        r["ISBN"].ToString());
+                }
+
+            }
+        }
+
+        static void CreateBooksTable()
+        {
+            string createTable = "create table Books (TITLE varchar(50), " +
+                "AUTHOR varchar(50), PUBDATE varchar(10), ISBN int)";
+            db.ExecuteNonQuery(createTable);
+        }
+    }
+}
---
+++ DatabaseSystemsCourse/07.ADO.NET/10.SQLLite/SQLiteHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+
+class SQLiteDatabase
+{
+    String dbConnection;
+
+    public SQLiteDatabase(String inputFile)
+    {
+        dbConnection = String.Format("Data Source={0}", inputFile);
+    }
+
+    public DataTable GetDataTable(string sql)
+    {
+        DataTable dt = new DataTable();
+        try
+        {
+            SQLiteConnection cnn = new SQLiteConnection(dbConnection);
+            cnn.Open();
+            SQLiteCommand mycommand = new SQLiteCommand(cnn);
+            mycommand.CommandText = sql;
+            SQLiteDataReader reader = mycommand.ExecuteReader();
+            dt.Load(reader);
+            reader.Close();
+            cnn.Close();
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+        return dt;
+    }
+
+    public int ExecuteNonQuery(string sql)
+    {
+        SQLiteConnection cnn = new SQLiteConnection(dbConnection);
+        cnn.Open();
+        SQLiteCommand mycommand = new SQLiteCommand(cnn);
+        mycommand.CommandText = sql;
+        int rowsUpdated = mycommand.ExecuteNonQuery();
+        cnn.Close();
+        return rowsUpdated;
+    }
+
+    public bool Insert(String tableName, Dictionary<String, String> data)
+    {
+        String columns = "";
+        String values = "";
+        Boolean returnCode = true;
+        foreach (KeyValuePair<String, String> val in data)
+        {
+            columns += String.Format(" {0},", val.Key.ToString());
+            values += String.Format(" '{0}',", val.Value);
+        }
+        columns = columns.Substring(0, columns.Length - 1);
+        values = values.Substring(0, values.Length - 1);
+     
+        this.ExecuteNonQuery(String.Format("insert into {0}({1}) values({2});", tableName, columns, values));
+        
+        return returnCode;
+    }
+}
+

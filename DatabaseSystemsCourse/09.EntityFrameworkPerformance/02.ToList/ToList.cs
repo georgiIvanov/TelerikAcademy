09d03e128956b2b78e37b2f91df7e6fda09049f7
Georgi <georgi.ivanovtn@gmail.com>
2013-07-16 16:01:58 +0300

    Performace task 2

---
+++ DatabaseSystemsCourse/09.EntityFrameworkPerformance/02.ToList/ToList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelerikEntities;
+using System.Diagnostics;
+
+namespace _02.ToList
+{
+    class ToList
+    {
+        static void Main(string[] args)
+        {
+            TelerikAcademyEntities db = new TelerikAcademyEntities();
+            string firstResult, secondResult;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            var towns = db.Employees.ToList()
+                .Select(emp => emp.Address).ToList()
+                .Select(town => town.Town).ToList()
+                .Where(town => town.Name == "Sofia");
+
+            sw.Stop();
+            firstResult = sw.Elapsed.ToString();
+
+            
+
+            foreach (var town in towns)
+            {
+                Console.WriteLine(town.Name);
+            }
+
+            sw.Restart();
+            towns = db.Employees
+                .Select(emp => emp.Address)
+                .Select(town => town.Town)
+                .Where(town => town.Name == "Sofia");
+
+            foreach (var town in towns)
+            {
+                Console.WriteLine(town.Name);
+            }
+
+            sw.Stop();
+            secondResult = sw.Elapsed.ToString();
+
+            Console.WriteLine("Time with ToList: {0}", firstResult);
+            Console.WriteLine("Time without ToList: {0}", secondResult);
+        }
+    }
+}

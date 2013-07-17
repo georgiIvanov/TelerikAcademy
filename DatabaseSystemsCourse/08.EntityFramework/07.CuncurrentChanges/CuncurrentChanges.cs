86318008be514438bf34d1648a37923077520c5a
Georgi <georgi.ivanovtn@gmail.com>
2013-07-17 14:20:28 +0300

    entityFramework task 11

---
+++ DatabaseSystemsCourse/08.EntityFramework/07.CuncurrentChanges/CuncurrentChanges.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFramework;
+using EntityFramework.Library;
+using System.Data.Entity;
+
+namespace _07.CuncurrentChanges
+{
+    class CuncurrentChanges
+    {
+        static void Main(string[] args)
+        {
+            northwindEntities db1 = new northwindEntities();
+            northwindEntities db2 = new northwindEntities();
+
+            //Categories can be added simultaneously
+            for (int i = 0; i < 10; i++)
+            {
+                db1.Categories.Add(new Category()
+                {
+                    CategoryName = "NewCategory" + i
+                });
+                db2.Categories.Add(new Category()
+                {
+                    CategoryName = "NewCategory" + i
+                });
+                db1.SaveChanges();
+                db2.SaveChanges();
+            }
+
+            //Editin same entry
+            var firstEntry = db1.Categories.Where((x) => x.CategoryName == "NewCategory0").First();
+            firstEntry.CategoryName = "Lol";
+
+            var sameEntry = db2.Categories.Where(x => x.CategoryName == "NewCategory0").First();
+
+            db2.Categories.Remove(sameEntry);
+
+            //Removing an entry before editing it throws an exception
+            //db2.SaveChanges();
+            db1.SaveChanges();
+            
+
+            
+        }
+    }
+}
---
+++ DatabaseSystemsCourse/08.EntityFramework/11.UsersDB/UsersDB.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _11.UsersDB
+{
+    class UsersDB
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new UsersContext())
+            {
+                DeleteGroupsAndUsers(db);
+
+                AddUser(db, "Lol");
+
+                var query = from us in db.Users
+                            select us;
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine("Name: {0}, Group: {1}", item.UserName, item.GroupId);
+                }
+                foreach (var group in db.Groups)
+                {
+                    Console.WriteLine("GroupId:{0}, GroupName: {1}", group.GroupId, group.GroupName);
+                }
+            }
+
+
+
+        }
+
+        private static void AddUser(UsersContext db, string name)
+        {
+            var admins = db.Groups.Where(x => x.GroupName == "Admins").ToList();
+
+            if (admins.Count == 0)
+            {
+                db.Groups.Add(new Group()
+                {
+                    GroupName = "Admins"
+                });
+                db.SaveChanges();
+                admins = db.Groups.Where(x => x.GroupName == "Admins").ToList();
+            }
+
+            User user = new User()
+            {
+                UserName = name,
+                GroupId = admins[0].GroupId
+            };
+
+            db.Users.Add(user);
+            db.SaveChanges();
+        }
+
+        private static void DeleteGroupsAndUsers(UsersContext db)
+        {
+            var toDelete = from us in db.Users
+                           select us;
+            foreach (var item in toDelete)
+            {
+                db.Users.Remove(item);
+            }
+
+            var groupsToDelete = from gr in db.Groups
+                           select gr;
+            foreach (var item in groupsToDelete)
+            {
+                db.Groups.Remove(item);
+            }
+
+            db.SaveChanges();
+        }
+    }
+
+
+    public class Group
+    {
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+    }
+
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public int GroupId { get; set; }
+    }
+
+    public class UsersContext : DbContext
+    {
+        public virtual DbSet<Group> Groups { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}

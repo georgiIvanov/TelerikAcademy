9a81abc239fba47e1f11aa49860743e6bd168653
Georgi <georgi.ivanovtn@gmail.com>
2013-08-09 17:57:15 +0300

    Tech Support - FormatConvert - Code Library

---
+++ TechnicalSupport/04.FormatConvert/ConsoleTestingApp/Program.cs
@@ -0,0 +1,68 @@
+﻿using FormatConvert;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleTestingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // convert xml string to json string
+            string personJson = FormatConverter.XmlToJsonAsString(xmlString);
+            Console.WriteLine("{0}\n", personJson);
+
+
+            // convert xml string to JObject
+            JObject jobj = FormatConverter.XmlToJsonAsJObject(xmlString);
+            Console.WriteLine(jobj);
+
+
+            // convert json string to XmlDocument
+            XmlDocument personXml = FormatConverter.JsonToXml(personJson);
+            PrintXML(personXml);
+
+            // convert xml document to json
+            string json = FormatConverter.XmlToJsonAsString(personXml);
+            Console.WriteLine("\n{0}\n", json);
+            
+
+            // convert JObject to Xml Document
+            XmlDocument result = FormatConverter.JsonToXml(jobj);
+            PrintXML(result);
+        }
+
+        private static void PrintXML(XmlDocument personXml)
+        {
+            foreach (XmlNode node in personXml)
+            {
+                foreach (XmlNode inner in node.ChildNodes)
+                {
+                    Console.WriteLine("{0}: {1}", inner.Name, inner.InnerText);
+                }
+            }
+        }
+
+        const string xmlString = @"<person>
+  <firstName>John</firstName>
+  <lastName>Smith</lastName>
+  <age>25</age>
+  <address>
+    <streetAddress>21 2nd Street</streetAddress>
+    <city>New York</city>
+    <state>NY</state>
+    <postalCode>10021</postalCode>
+  </address>
+  <phoneNumbers>
+    <phoneNumber type=""home"">212 555-1234</phoneNumber>
+    <phoneNumber type=""fax"">646 555-4567</phoneNumber>
+  </phoneNumbers>
+</person>";
+    }
+}
---
+++ TechnicalSupport/04.FormatConvert/FormatConvert/FormatConverter.cs
@@ -0,0 +1,121 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FormatConvert
+{
+    /// <summary>
+    /// Provides static methods for convertion from json to xml and vice-versa
+    /// </summary>
+    public static class FormatConverter
+    {
+        /// <summary>
+        /// Converts JSON to Xml Document
+        /// </summary>
+        /// <param name="documentToConvert">JSON as string</param>
+        /// <returns>XML Document object</returns>
+        public static XmlDocument JsonToXml(string documentToConvert)
+        {
+            if (string.IsNullOrWhiteSpace(documentToConvert))
+            {
+                throw new InvalidOperationException("You must first provide document as string");
+            }
+
+            XmlDocument doc = (XmlDocument)JsonConvert.DeserializeXmlNode(documentToConvert);
+
+            return doc;
+        }
+
+        /// <summary>
+        /// Converts JSON to Xml Document
+        /// </summary>
+        /// <param name="jsonObject">JSON as JObject</param>
+        /// <returns>XML Document object</returns>
+        public static XmlDocument JsonToXml(JObject jsonObject)
+        {
+            if (jsonObject == null)
+            {
+                throw new InvalidOperationException("You must first provide document as JObject");
+            }
+
+            return JsonToXml(jsonObject.ToString());
+        }
+
+
+        /// <summary>
+        /// Converts XML string to JSON
+        /// </summary>
+        /// <param name="documentToConvert">XML as string</param>
+        /// <returns>JSON as string</returns>
+        public static string XmlToJsonAsString(string documentToConvert)
+        {
+            if (string.IsNullOrWhiteSpace(documentToConvert))
+            {
+                throw new InvalidOperationException("You must first provide document as string");
+            }
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(documentToConvert);
+            string jsonText = JsonConvert.SerializeXmlNode(doc);
+
+
+            return jsonText;
+        }
+
+        /// <summary>
+        /// Converts XML string to JSON
+        /// </summary>
+        /// <param name="documentToConvert">XML Document</param>
+        /// <returns>JSON as string</returns>
+        public static string XmlToJsonAsString(XmlDocument doc)
+        {
+            if (doc == null)
+            {
+                throw new InvalidOperationException("You must first provide document as XmlDocument");
+            }
+            string jsonText = JsonConvert.SerializeXmlNode(doc);
+
+            return jsonText;
+        }
+
+        /// <summary>
+        /// Converts XML string to JObject
+        /// </summary>
+        /// <param name="documentToConvert">XML as XmlDocument</param>
+        /// <returns>JObject</returns>
+        public static JObject XmlToJsonAsJObject(XmlDocument doc)
+        {
+            if (doc == null)
+            {
+                throw new InvalidOperationException("You must first provide document as XmlDocument");
+            }
+            string jsonText = JsonConvert.SerializeXmlNode(doc);
+
+            return JObject.Parse(jsonText);
+        }
+
+        /// <summary>
+        /// Converts XML string to JObject
+        /// </summary>
+        /// <param name="documentToConvert">XML as string</param>
+        /// <returns>JObject</returns>
+        public static JObject XmlToJsonAsJObject(string documentToConvert)
+        {
+            if (string.IsNullOrWhiteSpace(documentToConvert))
+            {
+                throw new InvalidOperationException("You must first provide document as XmlDocument");
+            }
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(documentToConvert);
+            string jsonText = JsonConvert.SerializeXmlNode(doc);
+
+            return JObject.Parse(jsonText);
+        }
+
+    }
+}

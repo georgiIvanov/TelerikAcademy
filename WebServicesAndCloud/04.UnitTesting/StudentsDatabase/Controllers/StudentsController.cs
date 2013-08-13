98f5bb2c919b7673911b56ac74b69536fa3ec024
Georgi <georgi.ivanovtn@gmail.com>
2013-08-13 11:38:30 +0300

    refactored serialization

--- WebServicesAndCloud/04.UnitTesting/StudentsDatabase/Controllers/StudentsController.cs
+++ WebServicesAndCloud/04.UnitTesting/StudentsDatabase/Controllers/StudentsController.cs
@@ -22,26 +22,22 @@ namespace StudentsDatabase.Controllers
         public IEnumerable<Student> Get()
         {
             var studentEntities =  this.studentsRepository.All().ToList();
-            var students =
-                                from stud in studentEntities
-                                select new Student()
-                                {
-                                    Id = stud.Id,
-                                    FirstName = stud.FirstName,
-                                    LastName = stud.LastName,
-                                    Age = stud.Age,
-                                    Grade = stud.Grade,
-                                    Marks = stud.Marks
-                                };
 
-            return students;
+
+            return GetEntities(studentEntities);
         }
 
         // GET api/student/5
         public Student Get(int id)
         {
-            return this.studentsRepository.Get(id);
+            var found = this.studentsRepository.Get(id);
+
+            var student = CreateStudentToReturn(found);
+
+            return student;
         }
+
+        
 
         // POST api/student
         public HttpResponseMessage Post(Student value)
@@ -92,6 +88,28 @@ namespace StudentsDatabase.Controllers
             else
             {
                 return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+        }
+
+        private static Student CreateStudentToReturn(Student found)
+        {
+            var student = new Student()
+            {
+                Id = found.Id,
+                FirstName = found.FirstName,
+                LastName = found.LastName,
+                Age = found.Age,
+                Grade = found.Grade,
+                Marks = found.Marks
+            };
+            return student;
+        }
+
+        IEnumerable<Student> GetEntities(List<Student> students)
+        {
+            foreach (var stud in students)
+            {
+                yield return CreateStudentToReturn(stud);
             }
         }
     }

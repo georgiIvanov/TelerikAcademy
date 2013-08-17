81dc0147f2a48b0d9b3157e06573b6e44a198f63
Georgi <georgi.ivanovtn@gmail.com>
2013-08-18 00:56:29 +0300

    added service hook, reorganized PaaS homework folder

---
+++ WebServicesAndCloud/06.PaaS/02.ServiceHook/CodeJewelsHook/Controllers/HookController.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Typesafe.Mailgun;
+
+namespace itGeorgeHook.Controllers
+{
+    public class HookController : ApiController
+    {
+        public void Post([FromBody] Dictionary<string, object> content)
+        {
+            var buildReport = JsonConvert.SerializeObject(content);
+
+            var mailgunKeyName = "key-71yata1c1lo30of2hlltk628nh8oznu0";
+
+            var mailClient = new MailgunClient("app16919.mailgun.org", ConfigurationManager.AppSettings[mailgunKeyName]);
+            mailClient.SendMail(new System.Net.Mail.MailMessage("CodeJewelsHook@AppHarbor.net", "georgi.ivanovtn@gmail.com")
+            {
+                Subject = "Build Report",
+                Body = buildReport
+            });
+            
+        }
+    }
+}

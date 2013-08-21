652b1df08846d8e365d0a9aa1c843350d56f47ec
Georgi <georgi.ivanovtn@gmail.com>
2013-08-21 21:04:33 +0300

    underscorejs homework

---
+++ JSFrameworks/01.Underscore.js/Tasks/Animals.js
@@ -0,0 +1,18 @@
+﻿function Animal(name, species, legCount) {
+    this.name = name;
+    this.species = species;
+    this.legCount = legCount;
+}
+
+var animals = [
+    new Animal("Dog", "mammal", 4),
+    new Animal("Centipede George", "insect", 100),
+    new Animal("Centipede WW2 survivor", "insect", 78),
+    new Animal("Human", "mammal", 2),
+    new Animal("Dolphin", "mammal", 0),
+    new Animal("Birdie", "bird", 2),
+    new Animal("Prairie dog", "mammal", 4),
+    new Animal("Angry Bird", "bird", 0),
+    new Animal("Butterfly", "insect", 6),
+    new Animal("Teenage Mutant Ninja Bird", "bird", 4)
+];

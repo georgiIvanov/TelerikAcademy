9b5dd8172a58eb72636c76dd39700b24a1d622ea
Georgi <georgi.ivanovtn@gmail.com>
2013-08-22 19:35:10 +0300

    modernizr homework - all tasks

---
+++ JSFrameworks/03.Modernizr.js/Homework/Scripts/datepick-polyfill.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="jquery-ui-1.10.3.js" />
+
+var datePickerFallback = (function () {
+    function fallback() {
+        //var wrapper = document.getElementById("dateTimePicker");
+
+        //var image = document.createElement("img");
+        //image.src = "../Scripts/sadpanda.jpg"
+        $(function () {
+            $("#dateTimePicker").datepicker();
+        });
+        
+        //wrapper.appendChild(image);
+    }
+
+    return {
+        fallback: fallback
+    }
+})();
+
+datePickerFallback.fallback();
---
+++ JSFrameworks/03.Modernizr.js/Homework/Scripts/geo.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="jquery-2.0.3.js" />
+
+var geolocator = (function () {
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        }
+        else { x.innerHTML = "Geolocation is not supported by this browser."; }
+    }
+    function showPosition(position) {
+        var string = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+        $("#content").append(string);
+    }
+
+    return {
+        getLocation: getLocation,
+        showPosition: showPosition,
+    }
+}());
+
+geolocator.getLocation();

5ead890581ff1d824ddf46d64d5906725d6844ee
Georgi <georgi.ivanovtn@gmail.com>
2013-08-22 13:54:56 +0300

    moustachejs first task - table view template

---
+++ JSFrameworks/02.Moustache.js/Homework/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Homework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Homework")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("df0cccf8-b818-48b8-9c70-1cc125732a11")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ JSFrameworks/02.Moustache.js/Homework/Scripts/TableView.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="class.js" />
+var controls = controls || {};
+(function (c) {
+    var TableView = Class.create({
+        init: function (itemsSource) {
+            if (!(itemsSource instanceof Array)) {
+                throw "The itemsSource of TableView must be an array!";
+            }
+            this.itemsSource = itemsSource;
+        },
+        render: function (template) {
+            var table = document.createElement("table");
+            for (var i = 0; i < this.itemsSource.length; i++) {
+                var listItem = document.createElement("tr");
+                var item = this.itemsSource[i];
+                listItem.innerHTML = template(item);
+                table.appendChild(listItem);
+
+            }
+            return table.outerHTML;
+        }
+    });
+
+    c.getTableView = function (itemsSource) {
+        return new TableView(itemsSource);
+    }
+
+}(controls || {}));

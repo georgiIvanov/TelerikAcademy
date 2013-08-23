ab98f1f1aaa712e8e41d08a02b071bde83285c1e
Georgi <georgi.ivanovtn@gmail.com>
2013-08-23 19:53:56 +0300

    fixed geo location

--- JSFrameworks/03.Modernizr.js/Homework/Scripts/geo-polyfill.js
+++ JSFrameworks/03.Modernizr.js/Homework/Scripts/geo-polyfill.js
@@ -1,14 +1,58 @@
 ﻿/// <reference path="jquery-2.0.3.js" />
 var geolocatorFallback = (function () {
+    (function (geolocation) {
+
+        if (geolocation) return;
+
+        var cache;
+
+        geolocation = window.navigator.geolocation = {};
+        geolocation.getCurrentPosition = function (callback) {
+
+            if (cache) callback(cache);
+
+            $.getScript('//www.google.com/jsapi', function () {
+
+                // sometimes ClientLocation comes back null
+                if (google.loader.ClientLocation) {
+                    cache = {
+                        coords: {
+                            "latitude": google.loader.ClientLocation.latitude,
+                            "longitude": google.loader.ClientLocation.longitude
+                        }
+                    };
+                }
+
+                callback(cache);
+            });
+
+        };
+
+        geolocation.watchPosition = geolocation.getCurrentPosition;
+
+    })(navigator.geolocation);
+
+
+
+    // usage
+    
+   
+
     function fallback() {
         var image = document.createElement("img");
         image.src = "../Scripts/sadpanda.jpg"
 
-       
+        navigator.geolocation.watchPosition(function (pos) {
+            var element = document.createElement('div');
+            var string = "I'm located at " + pos.coords.latitude + ' and ' + pos.coords.longitude;
 
-        var wrapper = document.getElementById("content");
-        wrapper.appendChild(image);
-    }
+            
+
+            var wrapper = document.getElementById("content");
+            wrapper.appendChild(image);
+            wrapper.innerHTML += string;
+        })
+        };
 
    
     return {

e258bb057c4d04898dad571e20df6520262ceff2
Georgi <georgi.ivanovtn@gmail.com>
2013-08-24 14:06:36 +0300

    ComboBox task done

---
+++ JSFrameworks/04.RequireJS/02.ComboBox/Scripts/app-config.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="libs/class.js" />
+/// <reference path="libs/jquery-2.0.3.js" />
+/// <reference path="libs/mustache.js" />
+/// <reference path="libs/require.js" />
+
+
+require.config({
+    paths: {
+        jquery: "libs/jquery-2.0.3",
+        "class": "libs/class",
+        mustache: "libs/mustache",
+        controller: "app/controller"
+    }
+});
+
+require(["jquery", "controller", "mustache"], function ($, controller, mustache) {
+
+    var people = [
+  { id: 1, name: "Doncho Minkov", age: 18, avatarUrl: "../Images/minkov.jpg" },
+  { id: 2, name: "Georgi Georgiev", age: 19, avatarUrl: "../Images/joreto.jpg" },
+    { id: 3, name: "Leela", age: 21, avatarUrl: "../Images/leela.jpg" }];
+
+    var comboBox = controller.createComboBox(people);
+
+    var peopleTemplate = mustache.compile(document.getElementById("person-template").innerHTML);
+
+    var comboBoxHTML = comboBox.render(peopleTemplate);
+
+    $("#comboBox").append(comboBoxHTML);
+
+});
---
+++ JSFrameworks/04.RequireJS/02.ComboBox/Scripts/app/controller.js
@@ -0,0 +1,52 @@
+﻿/// <reference path="../libs/jquery-2.0.3.js" />
+/// <reference path="../libs/require.js" />
+/// <reference path="../libs/class.js" />
+
+define(["class", "jquery", "mustache"], function (Class, $, mustache) {
+    var controller = controller || {};
+
+    var ComboBox = Class.create({
+        init: function (itemsSource) {
+            this.itemsSource = itemsSource;
+        },
+        render: function (template) {
+            var comboBoxContainer = document.createElement("div");
+
+            var listItem = document.createElement('div');
+            var item = this.itemsSource[0];
+            listItem.innerHTML = template(item);
+
+            listItem.className = "selected";
+
+            comboBoxContainer.appendChild(listItem);
+
+            $(comboBoxContainer).on("click", ".selected", function () {
+                $(this).removeClass("selected");
+                $(comboBoxContainer).children().addClass("shown").show();
+            });
+
+
+            $(comboBoxContainer).on("click", ".shown", function () {
+                $(comboBoxContainer).children().removeClass("shown").hide();
+
+                $(this).addClass("selected").show();
+            });
+
+            for (var i = 1; i < this.itemsSource.length; i++) {
+                var item = this.itemsSource[i];
+                var listItem =  document.createElement('div');
+                listItem.innerHTML = template(item);
+                listItem.style.display = "none";
+                comboBoxContainer.appendChild(listItem);
+            }
+
+            return comboBoxContainer;
+        }
+    });
+
+    controller.createComboBox = function (itemsSource) {
+        return new ComboBox(itemsSource);
+    }
+
+    return controller;
+});
